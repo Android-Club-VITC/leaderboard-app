5a1bb2feffb2ba384111819476777bcb24d1e0d2
sudeekshaganguli <sudeekshaganguli@gmail.com>
2021-09-25 23:41:21 +0530

    pie chart added

--- src/components/contributionsSection/index.jsx
+++ src/components/contributionsSection/index.jsx
@@ -1,46 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { VictoryPie } from "victory-native";
-
-
 const ContributionsSection = () => {
-  graphicData: [
-    { y: 10, x: '5%'},
-    { y: 90, x: '90%'},
-    { y: 50, x: '50%'},
-    { y: 20, x: '20%'},
-    { y: 70, x: '70%'},
+  graphicData = [
+    { y: 25, x: 'Tech'},
+    { y: 25, x: 'Operations'},
+    { y: 25, x: 'Content'},
+    { y: 25, x: 'Creative'},
     ]
-
+    graphicColor = ['#0066cc', '#00e6e6', '#b3edff', '#00b3b3']
     return (
-      <View style={styles.container}>
-          <Text style={[styles.titleText]} >Contributions</Text>
+      <View style={styles.titleText,styles.VictoryPie}>
+          <Text style={{right:'10%',fontSize:'30',fontFamily: "Arial",color: 'black',}} >Contributions</Text>
           <VictoryPie
-        data={this.state.graphicData}
+        data={graphicData}
         width={250}
         height={250}
-        innerRadius={50}
-        style={{
-        labels: {
-        fill: 'white', fontSize: 15, padding: 7,}, }}
+        innerRadius={70}
+        style={{labels: {fill: 'black', fontSize: 16, padding: 18,}, }}
+        colorScale={graphicColor}
         /> 
-
+        <Text style={{position: 'absolute',top: 150,left:'22%',color: '#808080',}}> Overall Rank</Text>
+        <Text style={{position: 'absolute',top: 165,left:'30%',color: '#000000',fontWeight: "bold",}}> 68</Text>
       </View>
     
     );
 };
 const styles = StyleSheet.create({
-  container: {
-    marginRight:100,
-  },
-  titleText: {
-    color: 'black',    
-    fontSize: 30,
-    fontFamily: "Arial",
-    
-    
-  },
-  
-});
+VictoryPie: {marginTop:400}});
 export default ContributionsSection;
 
