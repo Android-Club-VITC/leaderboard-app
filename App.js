338544f2d3b6e4a2cc0df3052658827dfb3ba9dd
Neelesh Ranjan Jha <57111920+Neeleshrj@users.noreply.github.com>
2021-09-26 22:48:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ContributionsSection from './src/components/contributionsSection';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ContributionsSection/>
-      
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
