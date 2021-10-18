42630d20c8600b26977dabd29f85fbef490dce13
Sabari Ganesh <64348740+SabariGanesh-K@users.noreply.github.com>
2021-10-18 18:59:45 +0530

    design Loader #31 added
    Loader file was added with logo of android club rotating in loop with help of Animation

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 import { Text, View, SafeAreaView, StyleSheet, StatusBar } from "react-native";
 
+
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
-
 const Loader = () => {
-    return (
-        <View style={styles.loader}>
-            <Text>I Am Loading</Text>
-        </View>
-    );
-}
+  spinValue = new Animated.Value(0);
+  Animated.loop(
+    Animated.timing(this.spinValue, {
+      toValue: 1,
+      duration: 3000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    })
+  ).start();
+  const spin = this.spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+  return (
+    <View style={styles.loader}>
+      <Text>I Am Loading</Text>
+      <Animated.Image
+        style={{ transform: [{ rotate: spin }] }}
+        source={{ uri: "../../assets/ac_logo.png" }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    loader: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default Loader;
